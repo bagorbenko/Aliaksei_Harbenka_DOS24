faa9b4d6d3d3c38faf72dca1fcc306840b4a749b
Aliaksei <ba.gorbenko@gmail.com>
2025-03-02 23:44:42 +0100

    Jenkins 3

--- Homework_Lesson27_Jenkins_1/main.tf
+++ Homework_Lesson27_Jenkins_1/main.tf
@@ -1,23 +1,23 @@
 provider "google" {
-  project = "oceanic-base-450915-k4"  # Укажите ваш GCP Project ID
-  region  = "europe-west3"
+  project = "oceanic-base-450915-k4"
+  region  = "europe-north1-b"
 }
 
-resource "google_compute_instance" "jenkins_vm" {
-  name         = "jenkins-vm"
-  machine_type = "e2-micro"
-  zone         = "europe-west3-c"
+resource "google_compute_instance" "jenkins_vm_med" {
+  name         = "jenkins-vm-med"
+  machine_type = "e2-medium"
+  zone         = "europe-north1-b"
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
   network_interface {
     network = "default"
     access_config {
-      nat_ip = "35.246.158.16"
+      nat_ip = "34.89.221.182"
     }
   }
 
